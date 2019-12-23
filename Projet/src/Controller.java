75d39ea852e0ae0dcc8d6447cc66d9502c4638cd
adrienPAVOINE <57793863+adrienPAVOINE@users.noreply.github.com>
2019-12-23 12:56:42 +0100

    adrien

--- Projet/src/Controller.java
+++ Projet/src/Controller.java
@@ -1,3 +1,24 @@
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class Controller {
+
+    @FXML
+    private void ouvrir() {
+
+    }
+
 }
