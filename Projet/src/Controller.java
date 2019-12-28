1e0e3c5cfff18e1817098d497fa998f697cb7337
adrienPAVOINE <57793863+adrienPAVOINE@users.noreply.github.com>
2019-12-28 15:30:26 +0100

    adrien
    améioration de l'interface avec debut d'intergration d'information

--- Projet/src/Controller.java
+++ Projet/src/Controller.java
@@ -1,24 +1,193 @@
+import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
+import javafx.stage.Window;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.text.DecimalFormat;
 
-public class Controller {
 
+public class Controller  {
+    @FXML
+    private Label diametre;
     @FXML
     private void ouvrir() {
 
+        createFormOpenSaveFile(true,canvas);
+        System.out.print("ic ");
+
     }
+
+
+    //objets graphiques représentant un cercle
+
+    public  Rectangle rectangle;
+    private BooleanProperty isNotCreate = new SimpleBooleanProperty(true);
+    private static BaseDeTweets bd;
+
+
+
+//definir la troupe des objets graphiques
+
+    Group root;
+    Pane canvas = new Pane();
+    public void start(Stage primaryStage) {
+        construireScene(primaryStage);
+    }
+
+    void construireScene(Stage primaryStage) {
+
+        Scene scene = new Scene(root, 500, 500);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void createFormOpenSaveFile(boolean open, Pane p) {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Label label_fichier = new Label("Nom du fichier : ");
+        grid.add(label_fichier, 0, 1);
+
+        TextField field_fichier = new TextField();
+        grid.add(field_fichier, 1, 1);
+
+        ButtonBar buttonBar = new ButtonBar();
+
+        Button addBut = new Button("Confirmer");
+        ButtonBar.setButtonData(addBut, ButtonBar.ButtonData.OK_DONE);
+
+        Button cancelBut = new Button("Annuler");
+        ButtonBar.setButtonData(cancelBut, ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        // Add buttons to the ButtonBar
+        buttonBar.getButtons().addAll(addBut, cancelBut);
+
+        grid.add(buttonBar,1,2);
+
+        Scene sceneForm = new Scene(grid, 350, 100);
+
+        // New window (Stage)
+        Stage windowForm = new Stage();
+        if(open) {
+            windowForm.setTitle("Ouvrir un fichier");
+        }else {
+            windowForm.setTitle("Enregistrer sous...");
+        }
+        windowForm.setScene(sceneForm);
+
+        // Set position of second window, related to primary window.
+        //windowForm.setX(primaryStage.getX() + (primaryStage.getWidth()/2 - sceneForm.getWidth()/2));
+        //windowForm.setY(primaryStage.getY() + (primaryStage.getHeight()/2 - sceneForm.getHeight()/2));
+        windowForm.show();
+
+        addBut.setOnAction((ActionEvent e) -> {
+            if(open) {
+                try{
+                    bd = new BaseDeTweets();
+                    long startTime = System.currentTimeMillis();
+                    //ArrayList<Integer> lErr = bd.ouvrir(field_fichier.getText());
+                    bd.ouvrir(field_fichier.getText());
+                    long endTime = System.currentTimeMillis();
+                    Graph<String, DefaultWeightedEdge> g = bd.getDirectedWeightedGraph();
+                    int numVertex = 0;
+                    int i =1;
+                    for(String s : g.vertexSet()){
+                        float total = 0;
+                        for(DefaultWeightedEdge dwe :g.incomingEdgesOf(s)){
+                            total += g.getEdgeWeight(dwe);
+                        }
+                        Circle circle = createCircle(total,s);
+                        double x = 0 + i * circle.getRadius();
+                        double y = 50;
+                        circle.relocate(x,y);
+                        numVertex++;
+                        if(total > 3000 && i < 50){
+                            p.getChildren().add(circle);
+                            i++;
+                        }
+                    }
+
+                    System.out.println("Total elapsed time in execution of method callMethod() is :"+ (endTime-startTime)/1000+" secondes");
+                    /*if(lErr.size() != 0){
+                        showAlert(Alert.AlertType.ERROR,primaryStage,"Read error","Les lignes suivantes sont au mauvais format "+lErr);
+                    }
+                    updateDataTableView(table);*/
+
+                    DecimalFormat df = new DecimalFormat("0.00000000");
+                    double densite = bd.getDensite();
+                    diametre.setText(String.valueOf(densite));
+                    //System.out.println("A partir de la base de tweet :");
+
+                   // System.out.println("Taille : "+bd.getTaille());
+                    //System.out.println("Ordre : "+bd.getOrdre());
+                    long time = System.currentTimeMillis();
+                  /*  System.out.println("A partir de la base du graph :");
+                    System.out.println("Densité : "+bd.getDensite());
+                    System.out.println("Taille : "+bd.getTaille());
+                    System.out.println("Ordre : "+bd.getOrdre());
+                    System.out.println("Diamètre : "+bd.getDiametre());
+                    System.out.println("Page Rank : "+bd.getPageRank(5));
+                    System.out.println("Degre Moyen : "+bd.getMeanDegree());
+                    System.out.println("Degre Moyen In : "+bd.getMeanDegreeIn());
+                    System.out.println("Degre Moyen Out : "+bd.getMeanDegreeOut());
+                    System.out.println("Centralité par degré : "+bd.getDegreeCentrality(5));*/
+                    BaseDeTweets.reportPerformanceFor("After affichage",time);
+                }
+                catch(IOException ioe) {
+                    showAlert(Alert.AlertType.ERROR,"Read error","Problème de lecture du fichier"+ioe);
+                }
+            }
+            windowForm.close();
+        });
+
+        cancelBut.setOnAction((ActionEvent e) -> {
+            windowForm.close();
+        });
+
+    }
+
+    private Circle createCircle(float poids, String name){
+        float radius = 1.0f;
+        Circle circle = new Circle();
+        if(poids > 0){
+            radius *= Math.sqrt(poids)/3;
+        }
+        circle.setRadius(radius);
+        circle.setFill(Color.BLUE);
+        circle.setAccessibleText(name);
+        return circle;
+    }
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
 
 }
